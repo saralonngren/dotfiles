34c7f9afaecd151bec7db524e9a982077eaf7528
Sara Lonngren <saralonngren@gmail.com>
2020-11-13 08:20:28 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -94,11 +94,6 @@ echo -----------------------------------
 echo I cant live without you ❤️
 echo -----------------------------------
 
-echo 🗝 SPOTIFY --------------------------------
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client
-
 echo 🗝 SLACK ----------------------------------
 sudo snap install slack --classic
 sudo apt-get update
